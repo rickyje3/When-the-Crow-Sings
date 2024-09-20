89d176f62f339b5fd908fe284f2432576b8f8477
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-09-20 14:36:36 -0400

    Slightly updated ServiceLocator class.

--- When the Crow Sings/Assets/Scripts/ServiceLocator.cs
+++ When the Crow Sings/Assets/Scripts/ServiceLocator.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class ServiceLocator
+
+public sealed class ServiceLocator
 {
     // A series of globally accessible objects. The Type is the key and the object is the value, meaning there can only be one object of a given type in _services.
     private readonly static Dictionary<Type, object> _services = new Dictionary<Type, object>();
