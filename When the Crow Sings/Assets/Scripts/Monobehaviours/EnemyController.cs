f4ed01a1547c72a6332ed50ec7c7da6526ddcf38
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-09-23 21:58:21 -0400

    Enemy Chasing Works!

--- When the Crow Sings/Assets/Scripts/Monobehaviours/EnemyController.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/EnemyController.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : StateMachineComponent
 {
     public MeshCollider sightCone;
+    public NavMeshAgent navMeshAgent;
 
     private void Awake()
     {
         stateMachine = new StateMachine();
         stateMachine.RegisterState(new EnemyPatrolState(this), "EnemyPatrolState");
-        stateMachine.Enter("EnemyPatrolState");
+        stateMachine.RegisterState(new EnemyChaseState(this), "EnemyChaseState");
+        stateMachine.Enter("EnemyChaseState");
+    }
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
     }
 }
--- When the Crow Sings/Assets/Scripts/StateMachine/EnemyStates/EnemyChaseState.cs
+++ When the Crow Sings/Assets/Scripts/StateMachine/EnemyStates/EnemyChaseState.cs
@@ -10,31 +10,31 @@ public class EnemyChaseState : EnemyState
 
     public override void FixedUpdate()
     {
-        throw new System.NotImplementedException();
+        s.navMeshAgent.destination = ServiceLocator.Get<PlayerController>().transform.position;
     }
 
     public override void OnDisable()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public override void OnEnable()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public override void StateEntered()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public override void StateExited()
     {
-        throw new System.NotImplementedException();
+
     }
 
     public override void Update(float deltaTime)
     {
-        throw new System.NotImplementedException();
+
     }
 }
