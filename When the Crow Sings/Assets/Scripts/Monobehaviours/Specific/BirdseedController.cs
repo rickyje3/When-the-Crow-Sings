4bff24df28147453c2e0eeb8064515ca4de4378d
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-09-25 23:19:48 -0400

    Fixed minor error

--- When the Crow Sings/Assets/Scripts/Monobehaviours/Specific/BirdseedController.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/Specific/BirdseedController.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class BirdseedController : StateMachineComponent
 {
     public Transform pfCrowsTemp;
+
+    private void Awake()
+    {
+        stateMachine = new StateMachine(this); // TODO: Figure out how to make this line "required" so it's type-safe and shows the error at compile-time.
+    }
 }
