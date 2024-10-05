f19cff0a1c4c6b57d2a894e749ed6a3e69e45c98
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-10-05 14:36:19 -0400

    They are Services Now

--- When the Crow Sings/Assets/Scripts/Monobehaviours/Specific/DialogueManager.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/Specific/DialogueManager.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueManager : MonoBehaviour
+public class DialogueManager : MonoBehaviour, IService
 {
-
+    private void Awake()
+    {
+        RegisterSelfAsService();
+    }
+    public void RegisterSelfAsService()
+    {
+        ServiceLocator.Register<DialogueManager>(this);
+    }
 }
--- When the Crow Sings/Assets/Scripts/Monobehaviours/Specific/GameStateManager.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/Specific/GameStateManager.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameStateManager : MonoBehaviour
+public class GameStateManager : MonoBehaviour, IService
 {
-    
+    private void Awake()
+    {
+        RegisterSelfAsService();
+    }
+    public void RegisterSelfAsService()
+    {
+        ServiceLocator.Register<GameStateManager>(this);
+    }
 }
