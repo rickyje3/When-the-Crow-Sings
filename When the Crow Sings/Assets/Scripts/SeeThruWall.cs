e7df134f1741bf264028bb48ceecd6096a5a92ad
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2024-10-19 14:16:16 -0400

    Accidentally deleted something pushing before saving to get back

---
+++ When the Crow Sings/Assets/Scripts/SeeThruWall.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeThruWall : MonoBehaviour
+{
+    public Material SeeThruMaterial; //assign to seethrumaterial
+    public Camera Camera;
+    public LayerMask mask; //assign to wall
+
+    public static int PosID = Shader.PropertyToID("_Position");
+    public static int SizeID = Shader.PropertyToID("_Size");
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Camera.transform.position - transform.position;
+        var ray = new Ray(transform.position, dir.normalized);
+
+        if (Physics.Raycast(ray, 3000, mask))
+        {
+            SeeThruMaterial.SetFloat(SizeID, 1);
+        }
+        else SeeThruMaterial.SetFloat(SizeID, 0);
+
+        var view = Camera.WorldToViewportPoint(transform.position);
+        SeeThruMaterial.SetVector(PosID, view);
+    }
+}
