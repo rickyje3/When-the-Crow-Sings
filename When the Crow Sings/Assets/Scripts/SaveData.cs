0bc405aa1e8a4c01b2b9a4add967d5b6f1195081
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-10-18 21:44:02 -0400

    Added SaveData class

---
+++ When the Crow Sings/Assets/Scripts/SaveData.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public static class SaveData
+{
+    public static Dictionary<string, bool> flagsDialogue = new Dictionary<string, bool>()
+    {
+        { "AngelTaskOffered",false },
+        { "AngelTaskCompleted",false },
+        { "AngelBaseComplete",false },
+        { "AngelExhausted",false },
+        { "BeauTaskOffered",false },
+        { "BeauTaskCompleted",false },
+        { "BeauBaseComplete",false },
+        { "BeauExhausted",false },
+        { "CalebTaskOffered",false },
+        { "CalebTaskCompleted",false },
+        { "CalebBaseComplete",false },
+        { "CalebExhausted",false },
+        { "FaridaIntroduction",false },
+        { "FaridaTaskOffered",false },
+        { "FaridaTaskCompleted",false },
+        { "FaridaBaseCompleted",false },
+        { "FaridaExhausted",false },
+        { "QuinnTaskOffered",false },
+        { "QuinnTaskCompleted",false },
+        { "QuinnBaseCompleted",false },
+        { "QuinnExhausted",false },
+        { "JazmyneTaskOffered",false },
+        { "JazmyneTaskCompleted",false },
+        { "JazmyneBaseComplete",false },
+        { "JazmyneExhausted",false },
+        { "FranciscoTaskOffered",false },
+        { "FranciscoTaskCompleted",false },
+        { "FranciscoBaseCompleted",false },
+        { "FranciscoExhausted",false },
+        { "YuleTaskOffered",false },
+        { "YuleTaskCompleted",false },
+        { "YuleBaseCompleted",false },
+        { "YuleExhausted",false },
+        { "PhilomenaTaskOffered",false },
+        { "PhilomenaTaskCompleted",false },
+        { "PhilomenaBaseCompleted",false },
+        { "PhilomenaExhausted",false },
+        { "TheodoreIntroduction",false },
+        { "TheodoreTaskOffered",false },
+        { "TheodoreTaskCompleted",false },
+        { "TheodoreBaseCompleted",false },
+        { "TheodoreExhausted",false },
+    };
+
+    public enum TimeOfDay{MORNING,AFTERNOON,NIGHT}
+    public static TimeOfDay timeOfDay = TimeOfDay.MORNING;
+
+    public static int day = 0;
+
+    public static int tasksCompleted = 0;
+}
