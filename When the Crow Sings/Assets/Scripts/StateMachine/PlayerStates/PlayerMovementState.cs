17184533c8f0f9f5a76825f063ff5356fdc1a713
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2024-10-23 21:33:15 -0400

    forgot the ui object

--- When the Crow Sings/Assets/Scripts/StateMachine/PlayerStates/PlayerMovementState.cs
+++ When the Crow Sings/Assets/Scripts/StateMachine/PlayerStates/PlayerMovementState.cs
@@ -134,13 +134,13 @@ public class PlayerMovementState : StateMachineState
 
     private void OnInteract(InputAction.CallbackContext context)
     {
-        if (context.performed)
-        {
-            if (s.qteInteract.playerInRange)
-            {
+       // if (context.performed)
+       // {
+            //if (s.qteInteract.playerInRange)
+            //{
                 //activate when interact key is pressed
                 s.qteInteract.ActivateTimingMeter();
-            }
-        }
+           // }
+       // }
     }
 }
