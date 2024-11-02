0ffb8ecb765d1d2b07a30d5ad97c47d574e0b083
rickyje3 <122560145+rickyje3@users.noreply.github.com>
2024-11-02 17:15:18 -0400

    Gulp

--- When the Crow Sings/Assets/Scripts/SubSceneContainer.cs
+++ When the Crow Sings/Assets/Scripts/SubSceneContainer.cs
@@ -1,11 +1,9 @@
-using ScriptableObjects;
+using Eflatun.SceneReference;
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.SearchService;
-using UnityEngine;
 [System.Serializable]
 public class SubSceneContainer
 {
-    public SceneAsset subScene; //or sceneasset or scene
+    public SceneReference subScene; //or sceneasset or scene
     public List<SubSceneLogicBase> subSceneLogics = new List<SubSceneLogicBase>();
 }
