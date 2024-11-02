39ee2234c3b66f00b9604f8fdd9f6188c4d66789
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-11-02 16:15:20 -0400

    Initial task base class

---
+++ When the Crow Sings/Assets/Scripts/Tasks/TaskBase.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+abstract public class TaskBase : ScriptableObject
+{
+    public string taskName;
+}
