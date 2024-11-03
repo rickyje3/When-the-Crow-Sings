e2cb09f26f4a05c539b6a9cf347c462f67e822ea
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-11-03 00:39:03 -0400

    List of buttons

--- When the Crow Sings/Assets/MainMenu.cs
+++ When the Crow Sings/Assets/MainMenu.cs
@@ -9,8 +9,24 @@ public class MainMenu : MonoBehaviour
 {
     public SceneReference mainScene;
 
+    public List<Button> sceneLoadButtonList;
+
+    private void Awake()
+    {
+
+        foreach (Button i in sceneLoadButtonList)
+        {
+            i.onClick.AddListener(() => OnSceneLoadButtonPressed(sceneLoadButtonList.IndexOf(i)));
+        }
+    }
+
     public void LoadMainScene()
     {
         SceneManager.LoadScene(mainScene.Name);
+    }
+
+    public void OnSceneLoadButtonPressed(int index)
+    {
+        Debug.Log(index);
     }
 }
