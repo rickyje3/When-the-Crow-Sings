b945ecb54336b8974f6193e9e91da6e6aad960d1
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-11-02 20:22:04 -0400

    Instantiate qte

--- When the Crow Sings/Assets/Scripts/Monobehaviours/QTEs/QTE_UI_Holder.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/QTEs/QTE_UI_Holder.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class QTE_UI_Holder : MonoBehaviour
 {
     public void LoadQte(QuickTimeEvent qte)
     {
-        Debug.Log(qte);
+        //Debug.Log(qte);
+        Instantiate(qte).transform.SetParent(transform,false);
+
     }
 }
