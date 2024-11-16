c56f6f5200c869265faa8bb905dbff857b4db537
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-11-15 22:59:57 -0500

    Add bool debug flags

--- When the Crow Sings/Assets/Scripts/Monobehaviours/SaveEditMenu.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/SaveEditMenu.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveEditMenu : MonoBehaviour
 {
     public GameObject intFlagPrefab;
     public GameObject intContentHolder;
+    public GameObject boolFlagPrefab;
+    public GameObject boolContentHolder;
     private void Awake()
     {
-        float height = 0;
+        float boolHeight = 0;
+        foreach (KeyValuePair<string, bool> i in SaveData.boolFlags)
+        {
+            AddBoolFlagPrefab(i);
+            boolHeight += 30+boolContentHolder.GetComponent<VerticalLayoutGroup>().spacing;
+            boolContentHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(0, boolHeight);
+        }
+
+        float intHeight = 0;
         foreach (KeyValuePair<string, int> i in SaveData.intFlags)
         {
             if (i.Key == "penguin_cult") continue;
             AddIntFlagPrefab(i);
-            height += 30;
-            intContentHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(0, height);
+            intHeight += 30 + intContentHolder.GetComponent<VerticalLayoutGroup>().spacing;
+            intContentHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(0, intHeight);
         }
     }
 
@@ -23,5 +34,11 @@ public class SaveEditMenu : MonoBehaviour
         GameObject ii = Instantiate(intFlagPrefab);
         ii.GetComponent<DebugIntFlagOption>().key = i.Key;
         ii.transform.SetParent(intContentHolder.transform, false);
+    }
+    void AddBoolFlagPrefab(KeyValuePair<string,bool> i)
+    {
+        GameObject ii = Instantiate(boolFlagPrefab);
+        ii.GetComponent<DebugBoolFlagOption>().key = i.Key;
+        ii.transform.SetParent(boolContentHolder.transform, false);
     }
 }
