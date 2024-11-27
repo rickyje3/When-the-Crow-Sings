8d003130fd485ed87ca8e290a64e503818d3b882
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2024-11-27 02:20:32 -0500

    Add detection for current input device type

--- When the Crow Sings/Assets/Scripts/Monobehaviours/ThrowTarget.cs
+++ When the Crow Sings/Assets/Scripts/Monobehaviours/ThrowTarget.cs
@@ -8,13 +8,6 @@ public class ThrowTarget : MonoBehaviour
     public float radius = 5.0f;
     private void Update()
     {
-        //Vector3 clampedMousePosition = CameraManager.mouseWorldPosition;
-        //clampedMousePosition.x = Mathf.Clamp(clampedMousePosition.x, player.position.x - radius, player.position.x + radius);
-        //clampedMousePosition.y = Mathf.Clamp(clampedMousePosition.y, player.position.y - radius, player.position.y + radius);
-        //clampedMousePosition.z = Mathf.Clamp(clampedMousePosition.z, player.position.z - radius, player.position.z + radius);
-
-        //transform.position = clampedMousePosition;
-
         Vector3 direction = CameraManager.mouseWorldPosition - player.position;
 
         if (direction.magnitude > radius)
