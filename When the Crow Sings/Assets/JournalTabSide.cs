0aa42d91b2a9cb250721a71832d7471db5f455c9
Paul-Griffin-1517-1 <paulgriffin1517@gmail.com>
2025-01-25 21:14:11 -0500

    Side tabs appear/disappear

--- When the Crow Sings/Assets/JournalTabSide.cs
+++ When the Crow Sings/Assets/JournalTabSide.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class JournalTabSide : JournalTab
 {
+    public bool isActivated = false;
     public override void SetActivateTab(bool activated)
     {
-
+        GetComponent<Animator>().SetBool("isActivated", activated);
+        isActivated = activated;
     }
 
     public override void ActivateTab()
     {
         Debug.Log("Ping!");
+        SetActivateTab(true);
     }
 
     public override void DeactivateTab()
     {
         Debug.Log("!gniP");
-    }    
+        SetActivateTab(false);
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(wait());
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1f);
+        SetActivateTab(!isActivated);
+        StartCoroutine(wait());
+    }
 }
